1d2f3613572b242f6c579c72311aedee746d748b
Kushagra Tiwari <kushagratiwari86@gmail.com>
2026-05-03 19:51:04 +0530

    Set up environment variable system for API URL

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@
 export const IS_AUTHENTICATED = false;
 
 // Change to 'https://blogging-website-backend-9gfs.onrender.com' for production
-export const API_BASE_URL = 'https://blogging-website-backend-9gfs.onrender.com';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
