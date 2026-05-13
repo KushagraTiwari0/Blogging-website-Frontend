6a03b1c1c6af0d1a3fb06f824568f37c639bb5f8
Kushagra Tiwari <kushagratiwari86@gmail.com>
2026-05-14 02:44:59 +0530

    Naem css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -82,7 +82,7 @@ function Navbar() {
                     alt="avatar" 
                     className="nav-avatar" 
                   />
-                  <span>{authUser?.username}</span>
+                  <span>{authUser?.username?.split(' ')[0]}</span>
                 </NavLink>
               </li>
             </>
