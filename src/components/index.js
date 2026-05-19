d0763cc4a5d786e555604a7fd527dd48182c82ef
Kushagra Tiwari <kushagratiwari86@gmail.com>
2026-05-19 13:46:45 +0530

    Bug fixed

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 
 export {default as GuestRoute} from './GuestRoute';
-export {default as AuthRoute} from './GuestRoute';
+export {default as AuthRoute} from './AuthRoute';
 export {default as Navbar} from './Navbar';
 export {default as FormErrors} from './FormErrors';
 export {default as TagsInput} from './TagsInput';
